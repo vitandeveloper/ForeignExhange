80fd372535080e80b85ed91491c86db5a6ef96df
marlon <vitandeveloper@gmail.com>
2017-12-12 13:13:01 -0300

    actualizacíon 3

--- ForeignExhange/ForeignExhange/ViewModels/MainViewModel.cs
+++ ForeignExhange/ForeignExhange/ViewModels/MainViewModel.cs
@@ -1,11 +1,41 @@
 ﻿
 namespace ForeignExhange.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using Models;
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using System;
+
     public class MainViewModel
     {
+        #region Propiedades
+        public string Amount { get; set; }
+        public ObservableCollection<Rate> Rates { get; set; }
+        public Rate SourceRate { get; set; }
+        public Rate TargetRate { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsEnable { get; set; }
+        public string Result { get; set; }
+        #endregion
         public MainViewModel()
         {
 
         }
+
+        #region
+        public ICommand ConvertCommand
+        {
+            get
+            {
+                return new RelayCommand(Convert);
+            }
+        }
+
+        void Convert()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
