1fbbad9b3a764829604edba8fc8a64b27c6c3394
marlon <vitandeveloper@gmail.com>
2017-12-12 15:51:33 -0300

    actualizacion 4

--- ForeignExhange/ForeignExhange/ViewModels/MainViewModel.cs
+++ ForeignExhange/ForeignExhange/ViewModels/MainViewModel.cs
@@ -1,29 +1,80 @@
 ﻿
 namespace ForeignExhange.ViewModels
 {
+    using ForeignExhange.Models;
     using GalaSoft.MvvmLight.Command;
-    using Models;
     using System.Collections.ObjectModel;
     using System.Windows.Input;
     using System;
+    using System.ComponentModel;
 
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        #region Eventes
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Attributes
+        bool _isRunning;
+        string _result;
+        #endregion
+
         #region Propiedades
         public string Amount { get; set; }
         public ObservableCollection<Rate> Rates { get; set; }
         public Rate SourceRate { get; set; }
-        public Rate TargetRate { get; set; }
-        public bool IsRunning { get; set; }
-        public bool IsEnable { get; set; }
-        public string Result { get; set; }
+        public Rate TargeteRate { get; set; }
+        public bool IsRunning
+        {
+            get
+            {
+                return _isRunning;
+            }
+            set
+            {
+                if(_isRunning != value)
+                {
+                    _isRunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsRunning)));
+                }
+            }
+        }
+        public bool IsEnabled { get; set; }
+        public string Result
+        {
+            get
+            {
+                return _result;
+            }
+            set
+            {
+                if(_result != value)
+                {
+                    _result = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Result)));
+                }
+            }
+         }
         #endregion
+
+        #region Constructors
         public MainViewModel()
         {
-
+            LoadRates();
         }
 
-        #region
+      
+        #endregion
+
+        #region Methods
+        void LoadRates()
+        {
+            IsRunning = true;
+            Result = "Cargando datos...";
+        }
+        #endregion
+
+        #region Command
         public ICommand ConvertCommand
         {
             get
@@ -31,11 +82,12 @@ namespace ForeignExhange.ViewModels
                 return new RelayCommand(Convert);
             }
         }
-
         void Convert()
         {
             throw new NotImplementedException();
         }
         #endregion
+
+
     }
 }
