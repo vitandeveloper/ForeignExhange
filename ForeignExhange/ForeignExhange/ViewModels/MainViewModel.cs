e53322f530454bd1324b08d10abe3e25a16a4383
marlon <vitandeveloper@gmail.com>
2017-12-13 12:40:37 -0300

    Actualizacion 5

--- ForeignExhange/ForeignExhange/ViewModels/MainViewModel.cs
+++ ForeignExhange/ForeignExhange/ViewModels/MainViewModel.cs
@@ -7,6 +7,10 @@ namespace ForeignExhange.ViewModels
     using System.Windows.Input;
     using System;
     using System.ComponentModel;
+    using System.Net.Http;
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+    using Xamarin.Forms;
 
     public class MainViewModel : INotifyPropertyChanged
     {
@@ -16,12 +20,28 @@ namespace ForeignExhange.ViewModels
 
         #region Attributes
         bool _isRunning;
+        bool _isEnabled;
         string _result;
+        ObservableCollection<Rate> _rates;
         #endregion
 
         #region Propiedades
         public string Amount { get; set; }
-        public ObservableCollection<Rate> Rates { get; set; }
+        public ObservableCollection<Rate> Rates
+        {
+            get
+            {
+                return _rates;
+            }
+            set
+            {
+                if(_rates != value)
+                {
+                    _rates = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Rates)));
+                }
+            }
+        }
         public Rate SourceRate { get; set; }
         public Rate TargeteRate { get; set; }
         public bool IsRunning
@@ -39,7 +59,21 @@ namespace ForeignExhange.ViewModels
                 }
             }
         }
-        public bool IsEnabled { get; set; }
+        public bool IsEnabled
+        {
+            get
+            {
+                return _isEnabled;
+            }
+            set
+            {
+                if(_isEnabled != value)
+                {
+                    _isEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsEnabled)));
+                }
+            }
+        }
         public string Result
         {
             get
@@ -62,15 +96,40 @@ namespace ForeignExhange.ViewModels
         {
             LoadRates();
         }
-
-      
         #endregion
 
         #region Methods
-        void LoadRates()
+        async void LoadRates()
         {
             IsRunning = true;
             Result = "Cargando datos...";
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("https://apiexchangerates.azurewebsites.net");
+                var controller = "/api/Rates";
+                var response = await client.GetAsync(controller);
+                var result = await response.Content.ReadAsStringAsync();
+                if(!response.IsSuccessStatusCode)
+                {
+                    IsRunning = false;
+                    Result = Result;
+                }
+
+                var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+                Rates = new ObservableCollection<Rate>(rates);
+
+                IsRunning = false;
+                IsEnabled = true;
+                Result = "Listo para convertir";
+            
+            }
+            catch
+            {
+                IsRunning = false;
+                Result = "Ocurrio algo al conectarnos con el servidor.";
+            }
+          
         }
         #endregion
 
@@ -82,9 +141,48 @@ namespace ForeignExhange.ViewModels
                 return new RelayCommand(Convert);
             }
         }
-        void Convert()
+        async void Convert()
         {
-            throw new NotImplementedException();
+           if(string.IsNullOrEmpty(Amount))
+           {
+                await Application.Current.MainPage.DisplayAlert("Alerta",
+                                                                "Ingrece un monto",
+                                                                "Ok");
+                return;
+            }
+
+            decimal amount = 0;
+            if(!decimal.TryParse(Amount, out amount))
+            {
+                await Application.Current.MainPage.DisplayAlert("Alerta",
+                                                                "Ingrece un monto numerico",
+                                                                "Ok");
+                return;
+            }
+
+            if(SourceRate == null)
+            {
+                await Application.Current.MainPage.DisplayAlert("Alerta",
+                                                                "Seleccione una taza origen",
+                                                                 "Ok");
+                return;
+            }
+
+            if (TargeteRate == null)
+            {
+                await Application.Current.MainPage.DisplayAlert("Alerta",
+                                                                "Seleccione una taza destino",
+                                                                "Ok");
+                return;
+            }
+
+            var amountConerted = amount / (decimal)SourceRate.TaxRate *
+                                         (decimal)TargeteRate.TaxRate;
+            Result = string.Format("{0} {1:C2} = {2} {3:C2}",
+                                    SourceRate.Code,
+                                    Amount,
+                                    TargeteRate.Code,
+                                    amountConerted);
         }
         #endregion
 
